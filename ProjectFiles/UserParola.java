bb36c5089c79ee6eaf52086b43e78fdc82a8f943
deliacostea <126463814+deliacostea@users.noreply.github.com>
2024-02-26 23:50:53 +0200

    Uploaded project files.

---
+++ ProjectFiles/UserParola.java
@@ -0,0 +1,20 @@
+package poli14;
+
+public class UserParola {
+
+	public String User;
+	public String Parola;
+
+	public UserParola() {
+		super();
+		User = null;
+		Parola = null;
+	}
+
+	public UserParola(String user, String parola) {
+		super();
+		User = user;
+		Parola = parola;
+	}
+
+}
